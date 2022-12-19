ae1e9e18d3c8087d7f8601a0046b9c834be2a585
Sveta <sveta_chuprina@inbox.ru>
2022-12-19 22:43:01 +0300

    Solution for task 0,1,3,5,7

---
+++ Sem1Task0/Program.cs
@@ -0,0 +1,17 @@
+﻿
+//Считываем данные с консоли
+string? inputLine = Console.ReadLine();
+
+//Проверяем, чтобы данные были не пустыми
+if(inputLine!=null)
+{
+    //Парсим введенное число
+    int inputNumber = int.Parse(inputLine);
+
+    //Находим квадрат числа
+    int outNuber = inputNumber*inputNumber;
+
+    //Выводим данные в консоль
+    Console.WriteLine(outNuber);
+
+}
---
+++ Sem1Task7/Program.cs
@@ -0,0 +1,16 @@
+﻿//Считываем данные с консоли
+string? inputLine = Console.ReadLine();
+
+//Проверяем, чтобы данные были не пустыми
+if (inputLine != null)
+{
+
+    //Парсим введенное число
+    int inputNumber = int.Parse(inputLine);
+
+    int lastDigit = inputNumber % 10;
+
+    
+    //Выводим данные в консоль
+    Console.WriteLine(lastDigit);
+}
