dd6f30bc2e3d62e54ad46f039487f8803fa3271c
Sveta <sveta_chuprina@inbox.ru>
2022-12-21 22:37:28 +0300

    Solution for task 9,11,12,14,16

---
+++ Sem2Task11/Program.cs
@@ -0,0 +1,13 @@
+﻿// Создаем экземпляр класса рандомайзер
+System.Random numberSintezator = new System.Random();
+
+//Получаем новое случайное число
+int num = numberSintezator.Next(100,1000);
+
+Console.WriteLine (num);
+
+// int firstNum = num/100;
+// int secondNum = num%10;
+
+// int result = (num/100)*10+num%10;
+Console.WriteLine ((num/100)*10+num%10);
---
+++ Sem2Task16/Program.cs
@@ -0,0 +1,33 @@
+﻿
+void TestSQRT(int i, int j)
+{
+    bool result = (i/j==j);
+    if (result==true)Console.WriteLine("Число "+i+" является квадратом числа "+j);
+    else Console.WriteLine("Число "+i+" не является квадратом числа "+j);
+
+}
+
+int number1 = int.Parse(Console.ReadLine()??"0");
+int number2 = int.Parse(Console.ReadLine()??"0");
+TestSQRT(number1,number2);
+TestSQRT(number2,number1);
+
+
+
+// Console.WriteLine(number1);
+
+// bool result1 = (numberA / numberB == numberB);
+// bool result2 = (numberB / numberA == numberA);
+
+// if (result1) 
+
+// Console.WriteLine("Первое число квадрат второго числа!");
+
+// if (result2)
+
+// Console.WriteLine("Второе число квадрат первого числа!");
+
+// if (result2 && result1) 
+// {
+//     Console.WriteLine("Ни первое ни второе число не являються квадратами!");
+// }
