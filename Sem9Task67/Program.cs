b441d0f3bc28c8f7c25d000d591127205320710c
Sveta <sveta_chuprina@inbox.ru>
2023-01-15 16:16:30 +0300

    Solution for task 63, 65, 67, 69

---
+++ Sem9Task67/Program.cs
@@ -0,0 +1,29 @@
+﻿// ---------------------------------------------------------------------
+//                                 Задача 67
+// примает на вход число и возвращает сумму цифр в числе
+// ---------------------------------------------------------------------
+
+int numberN = ReadData("Enter number N:");
+int Sum = RecSumDig(numberN);
+PrintResult(Sum);
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Печать результата
+void PrintResult(int prefix)
+{
+    Console.Write(prefix);
+}
+
+//Рекурентный метод подсчета суммы цифр
+int RecSumDig(int n)
+{
+    if (n<= 10) return n;
+    int result = n%10 + RecSumDig(n/10);
+    return result;
+}
